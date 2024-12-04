1fbb4fc8977b4c8827f05f9ce6c501283a3cd303
Lidia C. L. P. <0000110806713xsp@al.educacao.sp.gov.br>
2024-12-03 21:52:15 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const avanca = document.querySelectorAll('.btn-proximo');
+
 avanca.forEach(button => {
     button.addEventListener(`click`, function() {
         const atual = document.querySelector('.ativo');
